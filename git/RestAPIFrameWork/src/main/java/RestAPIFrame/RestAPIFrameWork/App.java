739a63819b5cc47b920554a8f5ca6c035f9f0394
Vinayak <vinayaksk110@gmail.com>
2022-07-19 23:00:27 +0530

    main

---
+++ git/RestAPIFrameWork/src/main/java/RestAPIFrame/RestAPIFrameWork/App.java
@@ -0,0 +1,13 @@
+package RestAPIFrame.RestAPIFrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
